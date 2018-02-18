dd02dd6909e25476228aa73298d92a68c822f2c2
Gabriel Florit <gabrielflorit@gmail.com>
2018-02-18 01:02:37 -0500

    Format

--- src/containers/Editor.js
+++ src/containers/Editor.js
@@ -5,21 +5,14 @@ import { connect } from 'react-redux'
 // import TerminalInput from './../components/TerminalInput.js'
 // import actions from './../actions/actions.js'
 
-const mapStateToProps = state => ({
-  
-})
+const mapStateToProps = state => ({})
 
 const mapDispatchToProps = dispatch => ({
   // onInput: input => dispatch(actions.inputTerminalCommand(input))
 })
 
-const Editor = ({ history, onInput }) => (
-  <div className='Editor'>
-    editor
-  </div>
-)
+const Editor = ({ history, onInput }) => <div className='Editor'>editor</div>
 
-Editor.propTypes = {
-}
+Editor.propTypes = {}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Editor)
