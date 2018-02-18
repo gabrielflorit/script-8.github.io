888cc00d1601486d20b6c6e8606b1cc8460962d6
Gabriel Florit <gabrielflorit@gmail.com>
2018-02-18 01:02:24 -0500

    Add instructions to intro

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -1,7 +1,12 @@
+import makeOutput from './../utils/makeOutput.js'
+
 const initialState = {
   terminalHistory: [
     {
       output: 'Hello. My name is GAM-8. Welcome!'
+    },
+    {
+      output: makeOutput('help')
     }
   ]
 }
