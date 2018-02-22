9311279a48f393664695db9f1b6b0ac8c4cf1c90
Gabriel Florit <gabrielflorit@gmail.com>
2018-02-22 02:17:15 -0500

    Another screen

--- src/utils/boot.js
+++ src/utils/boot.js
@@ -20,37 +20,41 @@ function random(min, max) {
 
 const d = 2
 rectFill(0, 0, 128, 128, 7)
-let i = 0
-let j = 0
+let y = 0
 
 function update() {
-  i = Math.floor(j / 2) * 2
-  if (i <= 30) {
-    j++
+  if (y <= 32) {
+    y++
   }
 }
 
-const times = [...new Array(16)]
+const xs = [...new Array(128)]
 
 function draw() {
-  // times.forEach(_ => {
-  //   const x = random(0, 31) * d * d
-  //   const y = random(0, 31) * d * d
-  //   const c = colors[random(0, colors.length - 1)]
-  //   rectFill(x, y, d, d, c)
-  // })
-  // times.forEach(_ => {
-  //   const x = random(0, 128)
-  //   const y = random(0, 128)
-  //   rectFill(x, y, 1, 1, x)
-  // })
-  times.forEach(_ => {
-    const x = random(0, i) * d * d
-    const y = random(0, i) * d * d
-    const c = colors[random(0, colors.length - 1)]
-    rectFill((64 + i * d) - x, (64 + i * d) - y, d, d, c)
+  xs.forEach((_, x) => {
+    rectFill(x, y + 32 * 0, 1, 1, x)
+    rectFill(x, y + 32 * 1, 1, 1, x)
+    rectFill(x, y + 32 * 2, 1, 1, x)
+    rectFill(x, y + 32 * 3, 1, 1, x)
   })
+  // rectFill(random(0, 128), y, 1, 1, 7)
 }
+
+// function update() {
+//   y = j
+//   if (y <= 64) {
+//     j++
+//   }
+// }
+
+// const xs = [...new Array(64)]
+
+// function draw() {
+//   xs.forEach((_, x) => {
+//     rectFill(x*2, y*2, 2, 2, x)
+//   })
+//   rectFill(random(0, 64) * 2, y*2, 2, 2, 7)
+// }
 `
 
 export default boot
