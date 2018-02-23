cd1885afbcb991507aeb2e9cc4f0a8e1e924c5d6
Gabriel Florit <gabrielflorit@gmail.com>
2018-02-22 19:58:11 -0500

    another

--- src/utils/boot.js
+++ src/utils/boot.js
@@ -1,42 +1,32 @@
 const boot = `
-const colors = [
-  3,
-  7
-]
-
 function random(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const d = 2
 rectFill(0, 0, 128, 128, 7)
 
-let y = 0
+// first create the positions,
+// then cycle coloring the pixels
+)]
+  .map(_ => [random(0, 64) * 2,random(0, 64) * 2].join(','))
+))
+
+console.log(rects.length)
+
+let i = 0
 function update() {
-  if (y < 128) y++
+  i++
 }
 
-let count = [...new Array(8)]
-
-let rects = count.map(_ => [0, 0])
-
 function draw() {
-  // every tick,
-
-  // draw the previous rects blue
-  rects.forEach(rect => {
-    rectFill(rect[0], rect[1], 1, 1, 6)
-  })
-
-  // set coords of new rects,
-  rects = rects.map(_ => [random(0, 64) * 2, y])
-
-  // and draw the new ones white
-  rects.forEach(rect => {
-    rectFill(rect[0], rect[1], 1, 1, 3)
-  })
+  if (i % 4 === 0) {
+    rects.forEach(rect => {
+      const [x, y] = rect.split(',')
+      rectFill(x, y, 1, 1, random(5, 6))
+    })
+  }
 }
 `
 
