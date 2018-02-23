77994b342c0d854bc5e4c5260da82b8413267515
Gabriel Florit <gabrielflorit@gmail.com>
2018-02-23 18:23:22 -0500

    Add Output to Run component

--- src/components/Run.js
+++ src/components/Run.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Output from './Output.js'
 
-const Run = () => {
-  return <div className='Run'>run</div>
+const Run = ({ game, run }) => {
+  return (
+    <div className='Run'>
+      <Output game={game} run />
+    </div>
+  )
+}
+
+Run.propTypes = {
+  game: PropTypes.string,
+  run: PropTypes.bool
 }
 
 export default Run
