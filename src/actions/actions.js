6fb03d37ec26f9b1c4f6d50fc6bd3ca5168873ec
Gabriel Florit <gabrielflorit@gmail.com>
2018-02-24 03:05:59 -0500

    Point to updated now

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -22,7 +22,7 @@ export const fetchGist = id => dispatch => {
   dispatch(actions.fetchGistRequest())
 
   return window
-    .fetch(`https://my-service-dqzpehqqcr.now.sh/${id}`)
+    .fetch(`https://my-service-aprcdrtrnj.now.sh/${id}`)
     .then(
       response => response.json(),
       error => console.log('An error occurred.', error)
