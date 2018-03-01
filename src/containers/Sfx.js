b211cf0776868f26f1f3b57f25d21d650756579d
Gabriel Florit <gabrielflorit@gmail.com>
2018-03-01 01:35:22 -0500

    Figure out how to listen to mousedown + mousemove

--- src/containers/Sfx.js
+++ src/containers/Sfx.js
@@ -1,7 +1,5 @@
-import React from 'react'
-import classNames from 'classnames'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import _ from 'lodash'
 import Updater from './Updater.js'
 import Title from './Title.js'
 import Menu from './Menu.js'
@@ -11,22 +9,62 @@ const mapStateToProps = () => ({})
 
 const mapDispatchToProps = (dispatch, props) => ({})
 
-const Sfx = () => (
-  <div className='Sfx'>
-    <Updater />
-    <Title />
-    <Menu />
-    <NavBar />
-    <div className='temp'>
-      <ul className='bank'>
-        {_.range(1, 9).map(d => (
-          <li className={classNames({ active: d === 3 })} key={d}>
-            {d}
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
-)
+class Sfx extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleMouseMove = this.handleMouseMove.bind(this)
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.handleMouseUp = this.handleMouseUp.bind(this)
+  }
+
+  handleMouseMove (e) {
+    if (this.isDown) {
+      const { nativeEvent } = e
+      let offset
+      if ('offsetX' in nativeEvent) {
+        offset = [nativeEvent.offsetX, nativeEvent.offsetY]
+      } else {
+        const rect = e.target.getBoundingClientRect()
+        offset = [e.clientX - rect.left, e.clientY - rect.top]
+      }
+      console.log(offset)
+    }
+  }
+
+  handleMouseDown (e) {
+    this.isDown = true
+  }
+
+  handleMouseUp () {
+    this.isDown = false
+  }
+
+  noop () {}
+
+  render () {
+    return (
+      <div
+        className='Sfx'
+        onMouseUp={this.handleMouseUp}
+        onMouseDown={this.handleMouseDown}
+      >
+        <Updater />
+        <Title />
+        <Menu />
+        <NavBar />
+        <div className='temp'>
+          <div
+            className='pad'
+            ref={_pad => {
+              this._pad = _pad
+            }}
+            onMouseMove={this.handleMouseMove}
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sfx)
