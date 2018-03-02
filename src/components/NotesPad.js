2eaeb63c2ecc078a6f4de4a55ae40dacd55e382d
Gabriel Florit <gabrielflorit@gmail.com>
2018-03-02 01:15:29 -0500

    Draw bars on mouseover

--- src/components/NotesPad.js
+++ src/components/NotesPad.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import canvasAPI from '../utils/canvasAPI/index.js'
-// import _ from 'lodash'
+import _ from 'lodash'
 
 const size = 128
 
@@ -10,6 +10,24 @@ class NotesPad extends Component {
     super(props)
 
     this.handleMouseMove = this.handleMouseMove.bind(this)
+    this.drawBars = this.drawBars.bind(this)
+    this.drawBar = this.drawBar.bind(this)
+    this.state = {
+      bars: [1, 2, 3, 5, 8, 5, 3, 2]
+    }
+  }
+
+  drawBar (x, y) {
+    const d = 16
+    _.range(y).forEach(i => {
+      this.api.rectFill(
+        (x - 1) * d,
+        (8 - (i + 1)) * d,
+        d,
+        d,
+        Math.min(y - i - 1, 6)
+      )
+    })
   }
 
   componentDidMount () {
@@ -17,6 +35,14 @@ class NotesPad extends Component {
     const height = width
     const ctx = this._canvas.getContext('2d')
     this.api = canvasAPI({ ctx, width, height })
+    this.drawBars()
+  }
+
+  drawBars () {
+    this.api.clear()
+    this.state.bars.forEach((y, x) => {
+      this.drawBar(x + 1, y)
+    })
   }
 
   handleMouseMove (e) {
@@ -30,11 +56,20 @@ class NotesPad extends Component {
       } else {
         offset = [e.clientX - rect.left, e.clientY - rect.top]
       }
-      const [x, y] = offset
-
-      this.api.clear()
-      this.api.circFill(x * size / width, y * size / height, 10, 2)
-      this.api.circStroke(x * size / width, y * size / height, 10, 0)
+      const normalizedOffset = [
+        offset[0] * size / width,
+        offset[1] * size / height
+      ]
+      const positions = [
+        Math.ceil(normalizedOffset[0] / 16),
+        9 - Math.ceil(normalizedOffset[1] / 16)
+      ]
+      const bars = this.state.bars
+      bars[positions[0] - 1] = positions[1]
+      this.setState({
+        bars
+      })
+      this.drawBars()
     }
   }
 
@@ -43,6 +78,7 @@ class NotesPad extends Component {
   }
 
   render () {
+    console.log(Date.now())
     return (
       <canvas
         width={size}
