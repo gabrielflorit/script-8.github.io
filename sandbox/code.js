d64c9b636d28c53e38c456215b4e41dcc9734b7c
Gabriel Florit <gabrielflorit@gmail.com>
2018-03-05 02:37:32 -0500

    Style volume

--- sandbox/code.js
+++ sandbox/code.js
@@ -2,40 +2,55 @@ const s = 128
 
 const cols = 16
 const octaves = 3
-const halfStepHeight = 3
+const halfStepHeight = 2
+const totalNotes = octaves * 12 + 1
+const noteWidth = 5
+const volumeHeight = 2
+const notesVolumeGap = 8
 
 const x = d3.scaleLinear()
   .domain([1, cols + 1])
-  .range([0, 128])
+  .range([0, s])
 
-const y = d3.scaleLinear()
-  .domain([octaves * 12, 0])
-  .range([0, octaves * 12 * halfStepHeight])
+const gap = x(2) - x(1)
+
+const noteY = d3.scaleLinear()
+  .domain([totalNotes, 0])
+  .range([halfStepHeight - 1, (totalNotes + 1) * halfStepHeight - 1])
+
+const volumeY = d3.scaleLinear()
+  .domain([4, 1])
+  .range([noteY.range()[1] + notesVolumeGap, noteY.range()[1] + notesVolumeGap + 4*volumeHeight])
+
+const volumeColor = d3.scale
 
 // eslint-disable-next-line no-unused-vars
 function update () {}
 
-const gap = x(2) - x(1)
 const drawNote = ({ note, position, c = 6 }) => {
-  lineV(x(position) + gap/2, y(note), y(0) - y(note) + 1, 6)
-  rectFill(x(position) + gap/2 - Math.floor(gap/2) + 1, y(note), gap - 1, 1, 0)
+  lineV(x(position) + gap/2, noteY(note), noteY(0) - noteY(note) + 1, 6)
+  rectFill(x(position) + gap/2 - noteWidth/2 + 1, noteY(note) - halfStepHeight + 1, noteWidth, halfStepHeight, 0)
+}
+
+const drawVolume = ({ volume, position }) => {
+  console.log(volume)
+  rectFill(x(position) + gap/2 - noteWidth/2 + 1, volumeY(volume), noteWidth, volumeHeight, (4 - volume)*2)
 }
 
 clear()
 // rectStroke(0, 0, s, s, 6)
 
-let notes = range(cols - 2).map(d => random(1, octaves * 12))
-notes = [1].concat(notes).concat(octaves * 12)
-notes = [0, 1, 2, 3, 4, 5, 6, 7, 29, 30, 31, 32, 33, 34, 35, 36]
+let notes = [0, 1, 3, 5, 6, 8, 10, 12, 30, 31, 32, 33, 34, 35, 36, 37]
+// let notes = [0, 1, 3, 5, 6, 8, 10, 12, 13, 43, 44, 45, 46, 47, 48, 49]
+// notes = range(32)
 notes.forEach((note, position) => {
   drawNote({ note, position: position + 1 })
+  drawVolume({ volume: ((position + 3) % 4) + 1, position: position + 1 })
 })
+
+
+
 
 // eslint-disable-next-line no-unused-vars
 function draw () {
-
-  // notes.forEach((note, position) => {
-  //   drawNote({ note, position })
-  // })
-
 }
