6fc72bf4a4c7c2a33302cf5ea1e60c2b06348e74
Gabriel Florit <gabrielflorit@gmail.com>
2018-06-11 00:49:15 -0400

    Refactor

--- src/utils/isDirty.js
+++ src/utils/isDirty.js
@@ -5,21 +5,11 @@ import { extractGistPhrases } from '../reducers/phrases.js'
 import { extractGistChains } from '../reducers/chains.js'
 import { extractGistSongs } from '../reducers/songs.js'
 
-const isDirty = ({ gist, game, sprites, phrases, chains, songs }) => {
-  const gistGame = parseGistGame(gist.data)
-  const gistSprites = extractGistSprites(gist.data)
-  const gistPhrases = extractGistPhrases(gist.data)
-  const gistChains = extractGistChains(gist.data)
-  const gistSongs = extractGistSongs(gist.data)
-  const dirtyGame = !equal(gistGame, game)
-  const dirtySprites = !equal(gistSprites, sprites)
-  const dirtyPhrases = !equal(gistPhrases, phrases)
-  const dirtyChains = !equal(gistChains, chains)
-  const dirtySongs = !equal(gistSongs, songs)
-  const dirty =
-    dirtyGame || dirtyPhrases || dirtyChains || dirtySongs || dirtySprites
-
-  return dirty
-}
+const isDirty = ({ gist, game, sprites, phrases, chains, songs }) =>
+  !equal(parseGistGame(gist.data), game) ||
+  !equal(extractGistSprites(gist.data), sprites) ||
+  !equal(extractGistPhrases(gist.data), phrases) ||
+  !equal(extractGistChains(gist.data), chains) ||
+  !equal(extractGistSongs(gist.data), songs)
 
 export default isDirty
