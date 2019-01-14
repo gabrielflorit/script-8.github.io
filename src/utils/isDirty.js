5850b03a46813d564df8127a06ac393e08f1629f
Gabriel Florit <gabrielflorit@gmail.com>
2019-01-13 21:54:32 -0500

    Make isDirty more debuggable

--- src/utils/isDirty.js
+++ src/utils/isDirty.js
@@ -6,12 +6,33 @@ import { extractGistPhrases } from '../reducers/phrases.js'
 import { extractGistChains } from '../reducers/chains.js'
 import { extractGistSongs } from '../reducers/songs.js'
 
-const isDirty = ({ gist, game, sprites, map, phrases, chains, songs }) =>
-  !equal(parseGistGame(gist.data), game) ||
-  !equal(extractGistMap(gist.data), map) ||
-  !equal(extractGistSprites(gist.data), sprites) ||
-  !equal(extractGistPhrases(gist.data), phrases) ||
-  !equal(extractGistChains(gist.data), chains) ||
-  !equal(extractGistSongs(gist.data), songs)
+const isDirty = ({ gist, game, sprites, map, phrases, chains, songs }) => {
+  const gameIsDirty = !equal(parseGistGame(gist.data), game)
+  const mapIsDirty = !equal(extractGistMap(gist.data), map)
+  const spritesIsDirty = !equal(extractGistSprites(gist.data), sprites)
+  const phrasesIsDirty = !equal(extractGistPhrases(gist.data), phrases)
+  const chainsIsDirty = !equal(extractGistChains(gist.data), chains)
+  const songsIsDirty = !equal(extractGistSongs(gist.data), songs)
+
+  // console.log({ old: parseGistGame(gist.data), newer: game })
+
+  // console.log({
+  //   gameIsDirty,
+  //   mapIsDirty,
+  //   spritesIsDirty,
+  //   phrasesIsDirty,
+  //   chainsIsDirty,
+  //   songsIsDirty
+  // })
+
+  return (
+    gameIsDirty ||
+    mapIsDirty ||
+    spritesIsDirty ||
+    phrasesIsDirty ||
+    chainsIsDirty ||
+    songsIsDirty
+  )
+}
 
 export default isDirty
