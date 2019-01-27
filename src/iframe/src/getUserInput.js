a37c4a38e562b1a0290c73419cee679c828275e2
Gabriel Florit <gabrielflorit@gmail.com>
2019-01-26 23:30:06 -0500

    Add < and > to alphabet

--- src/iframe/src/getUserInput.js
+++ src/iframe/src/getUserInput.js
@@ -6,12 +6,8 @@ const getUserInput = keys => {
     right: keys.has('ArrowRight') || (buttons && buttons[15].pressed),
     down: keys.has('ArrowDown') || (buttons && buttons[13].pressed),
     left: keys.has('ArrowLeft') || (buttons && buttons[14].pressed),
-    a:
-      keys.has('a') ||
-      (buttons && (buttons[1].pressed || buttons[2].pressed)),
-    b:
-      keys.has('b') ||
-      (buttons && (buttons[0].pressed || buttons[3].pressed)),
+    a: keys.has('a') || (buttons && (buttons[1].pressed || buttons[2].pressed)),
+    b: keys.has('b') || (buttons && (buttons[0].pressed || buttons[3].pressed)),
     start: keys.has('Enter') || (buttons && buttons[9].pressed),
     select: keys.has(' ') || (buttons && buttons[8].pressed)
   }
