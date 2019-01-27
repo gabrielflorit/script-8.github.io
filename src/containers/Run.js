5ef2135706f5527c8cb1cfce6fc7e3bf417b4b29
Gabriel Florit <gabrielflorit@gmail.com>
2019-01-27 00:50:06 -0500

    Test mobile buttons

--- src/containers/Run.js
+++ src/containers/Run.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Run = () => (
-  <div className='Run'>
-  </div>
-)
+class Run extends Component {
+  constructor (props) {
+    super(props)
+    this.keydownA = this.keydownA.bind(this)
+    this.keyupA = this.keyupA.bind(this)
+    this.keydownRight = this.keydownRight.bind(this)
+    this.keyupRight = this.keyupRight.bind(this)
+  }
+
+  keydownA () {
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'a' }))
+  }
+  keyupA () {
+    document.dispatchEvent(new KeyboardEvent('keyup', { key: 'a' }))
+  }
+
+  keydownRight () {
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight' }))
+  }
+  keyupRight () {
+    document.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowRight' }))
+  }
+
+  render () {
+    return (
+      <div className='Run'>
+        <button
+          className='button'
+          onMouseDown={this.keydownA}
+          onMouseUp={this.keyupA}
+        >
+          A
+        </button>
+        <button
+          className='button'
+          onMouseDown={this.keydownRight}
+          onMouseUp={this.keyupRight}
+        >
+          Right
+        </button>
+      </div>
+    )
+  }
+}
 
 export default Run
