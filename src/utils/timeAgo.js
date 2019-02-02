1b564f4ed11997ea3d3bc250a40fd63fafc84f62
Gabriel Florit <gabrielflorit@gmail.com>
2019-02-02 15:51:52 -0500

    Lint

--- src/utils/timeAgo.js
+++ src/utils/timeAgo.js
@@ -12,11 +12,11 @@ const timeAgo = ({ now, before }) => {
   let diff = Math.round((now - before) / 1000)
   for (let t = 0; t < times.length; t++) {
     if (diff < times[t][1]) {
-      if (t == 0) {
+      if (t === 0) {
         return 'Just now'
       } else {
         diff = Math.round(diff / times[t - 1][1])
-        return diff + ' ' + times[t - 1][0] + (diff == 1 ? ' ago' : 's ago')
+        return diff + ' ' + times[t - 1][0] + (diff === 1 ? ' ago' : 's ago')
       }
     }
   }
