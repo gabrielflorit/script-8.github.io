db50f58c1654dd83ae67c0049e5ccdd0693ae45d
Gabriel Florit <gabrielflorit@gmail.com>
2019-02-09 12:14:02 -0500

    Update codemirror files

---
+++ updatecm.sh
@@ -0,0 +1,5 @@
+curl "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.43.0/codemirror.min.js" > public/codemirror/lib/codemirror.min.js
+curl "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.43.0/mode/javascript/javascript.min.js" > public/codemirror/mode/javascript/javascript.min.js
+curl "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.43.0/addon/comment/comment.min.js" > public/codemirror/addon/comment/comment.min.js
+curl "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.43.0/addon/lint/javascript-lint.min.js" > public/codemirror/addon/lint/javascript-lint.min.js
+curl "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.43.0/addon/lint/lint.min.js" > public/codemirror/addon/lint/lint.min.js
