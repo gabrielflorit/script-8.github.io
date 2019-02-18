fbd620fd5a02052ed221ef38ec94dceaba5ba95d
Gabriel Florit <gabrielflorit@gmail.com>
2019-02-18 15:10:39 -0500

    Format

--- src/components/ShelfCassettes.js
+++ src/components/ShelfCassettes.js
@@ -5,7 +5,7 @@ import timeAgo from '../utils/timeAgo.js'
 const STEP = 5
 
 class ShelfCassettes extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.renderCassette = this.renderCassette.bind(this)
     this.handleNext = this.handleNext.bind(this)
@@ -15,19 +15,19 @@ class ShelfCassettes extends Component {
     }
   }
 
-  handleNext () {
+  handleNext() {
     this.setState({
       index: this.state.index + STEP
     })
   }
 
-  handlePrevious () {
+  handlePrevious() {
     this.setState({
       index: Math.max(this.state.index - STEP, 0)
     })
   }
 
-  renderCassette ({ cassette, i, now }) {
+  renderCassette({ cassette, i, now }) {
     const { handleOnClick } = this.props
     const title = cassette.title || ''
     const maxLength = 16
@@ -40,64 +40,64 @@ class ShelfCassettes extends Component {
     const date = new Date(cassette.updated)
 
     return (
-      <li key={i} className='cassette'>
-        <div className='img'>
-          <span className='title'>{finalTitle || ' '}</span>
+      <li key={i} className="cassette">
+        <div className="img">
+          <span className="title">{finalTitle || ' '}</span>
           <a
             href={`/?id=${cassette.gist}`}
             onClick={e => {
               handleOnClick({ e, id: cassette.gist })
             }}
-            target='_blank'
+            target="_blank"
           >
-            <img className='background' alt='' src='./cassette-bg.png' />
-            <span className='load'>load cassette</span>
+            <img className="background" alt="" src="./cassette-bg.png" />
+            <span className="load">load cassette</span>
             {cassette.cover ? (
-              <img className='cover' src={cassette.cover} alt='' />
+              <img className="cover" src={cassette.cover} alt="" />
             ) : null}
           </a>
-          <span className='author'>by {cassette.user}</span>
-          <div className='date-info'>
-            <span className='date'>{timeAgo({ now, before: date })}</span>
-            <span className='booted'>
+          <span className="author">by {cassette.user}</span>
+          <div className="date-info">
+            <span className="date">{timeAgo({ now, before: date })}</span>
+            <span className="booted">
               {cassette.counter || 0} play{cassette.counter === 1 ? '' : 's'}
             </span>
           </div>{' '}
         </div>
-        <ul className='controls hide'>unshelve</ul>
+        <ul className="controls hide">unshelve</ul>
       </li>
     )
   }
 
-  render () {
+  render() {
     const { cassettes, title } = this.props
     const { index } = this.state
     const now = new Date()
 
     return (
-      <div className='ShelfCassettes'>
-        <div className='title-nav'>
-          <span className='title'>{title}</span>
+      <div className="ShelfCassettes">
+        <div className="title-nav">
+          <span className="title">{title}</span>
           <button
             disabled={index <= 0}
-            className='button'
+            className="button"
             onClick={this.handlePrevious}
           >
             &lt;
           </button>
           <button
             disabled={index >= cassettes.length - STEP}
-            className='button'
+            className="button"
             onClick={this.handleNext}
           >
             &gt;
           </button>
-          <span className='count'>
+          <span className="count">
             {index + 1} - {Math.min(index + STEP, cassettes.length)} (
             {cassettes.length} total)
           </span>
         </div>
-        <ul className='cassettes'>
+        <ul className="cassettes">
           {_(cassettes)
             .slice(index, index + STEP)
             .map((d, i) => this.renderCassette({ cassette: d, i, now }))
