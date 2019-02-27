f937ed6a5673cd0f4d36cbe0b5a9a7e51a76c19d
Gabriel Florit <gabrielflorit@gmail.com>
2019-02-27 02:40:33 -0500

    Disable site

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import { Provider } from 'react-redux'
 import App from './containers/App.js'
 import store from './store/store.js'
 
-window.script8 = window.script8 || {}
+// window.script8 = window.script8 || {}
+localStorage.removeItem('script-8')
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  // <Provider store={store}>
+  <div className="maintenance">
+    <p>
+      SCRIPT-8 is undergoing maintenance and will be back soon. In the meantime,
+      come join us on the{' '}
+      <a className="text" href="https://discord.gg/HA68FNX">
+        Fantasy Consoles Discord server
+      </a>
+      , a friendly place to chat about these sophisticated, cutting-edge
+      computers. The server has a dedicated SCRIPT-8 room.
+    </p>
+    <p>- Gabriel</p>
+  </div>,
+  // </Provider>,
   document.getElementById('root')
 )
