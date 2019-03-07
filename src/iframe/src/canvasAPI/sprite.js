973248dd5bfc97a92ee4cf280b64618f8a9aeaf4
Keith Simmons <keith@the-simmons.net>
2019-03-07 10:53:49 -0800

    Remove yet more semicolons

--- src/iframe/src/canvasAPI/sprite.js
+++ src/iframe/src/canvasAPI/sprite.js
@@ -5,11 +5,11 @@ const drawSprite = ({ x, y, spriteIndex, darken, flipH, flipV, setPixel, sprites
     sprites[spriteIndex].slice(0, 8).forEach((cells, rowIndex) => {
       cells.split('').forEach((color, colIndex) => {
         if (color !== ' ') {
-          const clamped = clamp(+color - darken, 0, 7);
+          const clamped = clamp(+color - darken, 0, 7)
           setPixel(
             Math.floor(x) + (flipH ? 7 - colIndex : colIndex),
             Math.floor(y) + (flipV ? 7 - rowIndex : rowIndex),
-            clamped);
+            clamped)
         }
       });
     });
