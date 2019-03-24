d7c846c9693ae4eabb5161cbe5c1f82aa004fb95
Gabriel Florit <gabrielflorit@gmail.com>
2019-03-24 01:39:37 -0400

    If no gist, use latest iframeVersion
    closes #276

--- src/iframe/src/gistParsers/iframeVersion.js
+++ src/iframe/src/gistParsers/iframeVersion.js
@@ -1,11 +1,15 @@
 import _ from 'lodash'
 import initialState from '../store/initialState.js'
 
-const parseGistIframeVersion = data =>
-  _.get(
-    JSON.parse(_.get(data, 'files["misc.json"].content', '{}')),
-    'iframeVersion',
-    '0.1.247'
-  )
+const parseGistIframeVersion = data => {
+  const iframeVersion = data
+    ? _.get(
+        JSON.parse(_.get(data, 'files["misc.json"].content', '{}')),
+        'iframeVersion',
+        '0.1.247'
+      )
+    : initialState.iframeVersion
+  return iframeVersion
+}
 
 export { parseGistIframeVersion }
