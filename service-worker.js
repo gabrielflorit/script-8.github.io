91498e07bf3a26f072e0d2e7b7ab2fea7fb4f1d9
Gabriel Florit <gabrielflorit@gmail.com>
2019-11-27 18:31:14 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.019aa6305fbf9941d0316d1056b13774.js"
+  "/precache-manifest.c1fc288a4af6baa50767a7d6e015fda7.js"
 );
 
 workbox.clientsClaim();
